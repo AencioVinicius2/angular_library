090f02e30aa1cbc2bae4efe079bc146ea8b0edb9
Aencio <aenpvp9@gmail.com>
2024-07-24 10:39:43 -0300

    add: card-roxo

---
+++ Section1/projeto-inicial/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardComponent } from './card/card.component'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'projeto-inicial';
+}
---
+++ Section1/projeto-inicial/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardRoxoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
