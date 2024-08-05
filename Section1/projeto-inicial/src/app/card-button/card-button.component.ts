7c64f4d311e5322d5ee5ccf4dba94d47b08b7419
Aencio <aenpvp9@gmail.com>
2024-08-05 12:16:39 -0300

    fix: fixing some issues

--- Section1/projeto-inicial/src/app/card-button/card-button.component.ts
+++ Section1/projeto-inicial/src/app/card-button/card-button.component.ts
@@ -2,21 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'card-button',
-  // templateUrl: './card-button.component.html',
-  template: `
-    <div class="card__button">R$250,0/Mês</div>
-  `,
-  styles: 
-  `
-  .card__button {
-    font-size: 16px;
-    text-align: center;
-    margin-top: 10px;
-    border-radius: 25px;
-    background-color: #FFFF;
-    cursor: pointer;
-  }
-  `
+  templateUrl: './card-button.component.html',
+  styleUrl: './card-button.component.css',
 })
 export class CardButtonComponent {
 
