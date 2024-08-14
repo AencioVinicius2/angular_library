6d8f87f3e4bd26daf743257079d6ed75f47a0ae2
Aencio <aenpvp9@gmail.com>
2024-08-14 13:37:20 -0300

    feat: add components style in project1

--- projeto1/users-filter-project/src/app/app.module.ts
+++ projeto1/users-filter-project/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule
+    ComponentsModule,
   ],
   providers: [
     provideClientHydration(),
--- projeto1/users-filter-project/src/app/components/components.module.ts
+++ projeto1/users-filter-project/src/app/components/components.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AngularMaterialModule } from '../angular-material/angular-material.module';
 
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
 @NgModule({
+  //--components standalone não precisam ser declarados--//
   declarations: [],
   imports: [
     CommonModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+  ],
+  exports: [
+    AngularMaterialModule,
   ]
 })
 export class ComponentsModule { }
