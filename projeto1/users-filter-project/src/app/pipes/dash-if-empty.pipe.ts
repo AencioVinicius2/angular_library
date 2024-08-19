2a5bf3039e4c6f799d1f1ba7e33533fcf1d16d9e
Aencio <aenpvp9@gmail.com>
2024-08-19 10:36:32 -0300

    fix: projeto1 dashIfEmpty returning nothing

--- projeto1/users-filter-project/src/app/pipes/dash-if-empty.pipe.ts
+++ projeto1/users-filter-project/src/app/pipes/dash-if-empty.pipe.ts
@@ -7,7 +7,7 @@ import { __values } from 'tslib';
 export class DashIfEmptyPipe implements PipeTransform {
   transform(value: any): string | any {
     let IS_EMPTY = value === undefined || value === null || value === '';
-    IS_EMPTY ? '-' : value;
+    return IS_EMPTY ? '-' : value;
   }
 
 }
