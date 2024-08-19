1afbb260394dd79e23077df759167464c16b3ce6
Aencio <aenpvp9@gmail.com>
2024-08-19 14:32:06 -0300

    feat: projeto add users-list output and input

--- projeto1/users-filter-project/src/app/app.component.ts
+++ projeto1/users-filter-project/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { IUser } from './interfaces/user/users.interface';
+import { Component, OnInit } from '@angular/core';
 import { UsersList } from './data/users-list';
-
+import { IUser } from './interfaces/user/users.interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  userSelected: IUser = UsersList[0];
+export class AppComponent implements OnInit {
+  usersList: IUser[] = [];
+  userSelected: IUser = {} as IUser;
+  showUserDetails: boolean = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.usersList = UsersList;
+    }, 3000)
+  }
+
+  onUserSelected(user: IUser) {
+    this.userSelected = user;
+    this.showUserDetails = true;
+  }
 }
--- projeto1/users-filter-project/src/app/components/users-list/users-list.component.ts
+++ projeto1/users-filter-project/src/app/components/users-list/users-list.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { IUser } from '../../interfaces/user/users.interface';
-import { UsersList } from '../../data/users-list';
-import { UserDetailsComponent } from '../user-details/user-details.component';
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss'
 })
 export class UsersListComponent {
-
   displayedColumns: string[] = ['name', 'date', 'status'];
-  usersList: IUser[] = UsersList;
+
+  @Input({ required: true }) usersList: IUser[] = [];
+
+  @Output('userSelected') userSelectedEmitt = new EventEmitter<IUser>();
 
   onUserSelected(user: IUser) {
-    console.log(user);
+    //console.log('user', user);
+    this.userSelectedEmitt.emit(user)
   }
-
 }
