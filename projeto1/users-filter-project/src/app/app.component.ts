7ecb93301489a47a4275c3e6fb3fd7f62b2649ae
Aencio <aenpvp9@gmail.com>
2024-08-20 13:26:38 -0300

    fix: projeto1 status filter do not return when has value in input name

--- projeto1/users-filter-project/src/app/app.component.ts
+++ projeto1/users-filter-project/src/app/app.component.ts
@@ -28,7 +28,6 @@ export class AppComponent implements OnInit {
 
   onFilter(filterOptions: IFilterOptions) {
     console.log(filterOptions);
-
     this.usersListFiltered = this.filterUsersList(filterOptions, this.usersList);
   }
 
@@ -36,19 +35,26 @@ export class AppComponent implements OnInit {
     let filteredList: IUser[] = [];
 
     filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+    filteredList = this.filterUsersLisyByStatus(filterOptions.status, filteredList);
+    return filteredList;
+  }
 
+  filterUsersLisyByStatus(status: boolean | undefined, usersList: IUser[]): IUser[] {
+    const STATUS_NOT_SELECTED = status === undefined;
+    console.log('status pressionado')
+    if(STATUS_NOT_SELECTED) {
+      return usersList;
+    }
+    const filteredList = usersList.filter((user) => user.ativo === status);
     return filteredList;
   }
 
   filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
     const NAME_NOT_TYPPED = name === undefined;
-
     if(NAME_NOT_TYPPED) {
       return usersList;
     }
-
     const filteredList = usersList.filter((user) => user.nome.toLowerCase().includes(name.toLowerCase()));
-
     return filteredList;
   }
 }
