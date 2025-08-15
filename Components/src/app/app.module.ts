bd210a94cf330f0a14cd873f9a6f02328787df83
Aencio <aenpvp9@gmail.com>
2025-08-15 14:26:16 -0300

    feat: card-roxo component

--- Components/src/app/app.module.ts
+++ Components/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardRoxoComponent
   ],
   imports: [
     BrowserModule,
---
+++ Components/src/app/card-roxo/card-roxo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-roxo',
+  templateUrl: './card-roxo.component.html',
+  styleUrl: './card-roxo.component.scss'
+})
+export class CardRoxoComponent {
+
+}
