b99f1db676b47b23684944b896d89122d74e391a
AencioVinicius2 <aenciosilva@gmail.com>
2025-08-18 15:18:35 -0300

    feat: change card-button template to inlane-template

--- Components/src/app/card-button/card-button.component.ts
+++ Components/src/app/card-button/card-button.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
-  templateUrl: './card-button.component.html',
+  //templateUrl: './card-button.component.html',
+  template: `
+    <div class="card-button card__item">Adquirir</div>
+
+  `,
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
