06f327cafcee93b7e2b75a67102bc4f69fb91e96
AencioVinicius2 <aenciosilva@gmail.com>
2025-08-18 14:48:50 -0300

    feat: card.component interfaces created

--- Components/src/app/card/card.component.ts
+++ Components/src/app/card/card.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 
+interface IPlano {
+  infos: IInfos;
+}
+
+interface IInfos {
+  tipo: string,
+  preco: number
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  plano = {
-    tipo: 'Simples',
-    preco: 'R$ ' + 100 + ',00/Mês',
-  }
+   // @ts-ignore
+  plano: IPlano = {
+    infos: {
+      tipo: 'Simples',
+      preco: 123
+    }
+  };
 }
